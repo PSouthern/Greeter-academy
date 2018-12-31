ddd7db1ed4388c0acf75c3bb418fc43fd15d2611
PaulSouthern <paul.southern@digital.hmrc.gov.uk>
2018-12-31 14:50:37 +0000

    adding additional data types, private method and refactoring

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -6,19 +6,22 @@ object Prompt {
   def ask(message: String) = StdIn.readLine(message)
 }
 
-class Person(val name: String) {
+class Person(val name: String, age: Int) {
+  private val years : String = if(age > 1) "years" else "year"
+
   def speak() : String = {
     if (name == "adam") {
       s"You don't get a hello!"
     }
     else {
-      s"Hello $name"
+      s"Hello $name, you are $age $years old"
     }
   }
 }
 
 object GreeterApplication extends App {
   val name = Prompt.ask("What is your name? ")
-  val person = new Person(name)
+  val age = Prompt.ask("How old are you? ")
+  val person = new Person(name, age.toInt)
   println(person.speak())
 }
